9f7d02193bb2630df46fdb2704d71f84b251ca8a
andrii.belon@gmail.com <andrii.belon@gmail.com>
2018-01-22 18:31:57 +0200

    next

--- Kivy - Interactive Applications and Games in Python Second Edition_Code/Chapter_01_code/02 - Basic interace - Hello World (hello2)/hello2.py
+++ Kivy - Interactive Applications and Games in Python Second Edition_Code/Chapter_01_code/02 - Basic interace - Hello World (hello2)/hello2.py
@@ -5,9 +5,11 @@ kivy.require('1.9.0')
 from kivy.app import App
 from kivy.uix.button import Label
 
+
 class Hello2App(App):
     def build(self):
         return Label()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Hello2App().run()
